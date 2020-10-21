3921ebce4286342892410acf53efec4b66d82a15
HallRB <Richardbrianhall@gmail.com>
2020-10-20 20:03:37 -0500

    added handlebars, parse, and route

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
-var path = require("path");
+// dependencies
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-var PORT = process.env.PORT || 3000;
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+// create express app instance.
+const app = express();
 
-var db = require("./models");
+// set Handlebars.
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application json
+app.use(bodyParser.json());
+
+// import routes and give them server access
+const routes = require("./controllers/burgerController.js");
+app.use(routes);
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
