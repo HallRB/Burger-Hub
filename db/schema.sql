1b0fa6fed0b398ff487ddfc0af43764c686025a6
HallRB <Richardbrianhall@gmail.com>
2020-10-20 20:47:35 -0500

    db

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,17 @@
+-- drops db if it already exists --
+DROP DATABASE IF EXISTS eataburger
+
+-- creates db "eatadburger"
 CREATE DATABASE eatdaburger;
+
+--use the db "eataburger":
 USE eatdaburger;
+
+--create table for db "eataburger"
+CREATE TABLE burgers
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
